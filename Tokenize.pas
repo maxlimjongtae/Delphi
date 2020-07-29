9497e0e6aed62e0d4fdb4ccd3dd2f33dc2a27e7f
max <max@chequer.io>
2020-07-30 02:09:05 +0900

    Refactor
    StatePattern Application of Tokenize

--- Tokenize.pas
+++ Tokenize.pas
@@ -2,40 +2,75 @@ unit Tokenize;
 
 interface
 
-uses System.SysUtils, System.Classes, System.TypInfo, Token, TokenList;
+uses
+  System.SysUtils, System.Classes, System.TypInfo, Token, TokenList;
 
 type
+  TState = function: Boolean of object;
+
   TTokenize = class
   private
     FTokenList: TTokenList;
 
     FValue: string;
-    FString: string;
     FIndex: Integer;
+
+    FCurrentState: TState;
+    FTemporaryValue: string;
+
+    function SemiColonState: Boolean;
+    function ColonState: Boolean;
+    function SpaceState: Boolean;
+    function EqualState: Boolean;
+    function OperatorState: Boolean;
+    function ReturnState: Boolean;
+    function UndefinedState: Boolean;
+
+    function InitialState: Boolean;
+    function BranchState: Boolean;
+    function FinalState: Boolean;
+
+    function CurrentValue: string;
   public
     constructor Create;
     destructor Destroy; override;
 
-    function Tokenize: TTokenList;
-    function CurrentValue: string;
+    function Execute(Value: string): TTokenList;
+
     function CanNext: Boolean;
-
-    function Print: string;
-
+    function ToString: string;
     procedure Next;
 
     property TokenList: TTokenList read FTokenlist write FTokenList;
-    property Value: string read FValue write FValue;
-    property Index: Integer read FIndex write FIndex;
+    property Value: string write FValue;
+    property Index: Integer read FIndex;
   end;
 
 implementation
 
 { TTokenize }
 
+function TTokenize.ColonState: Boolean;
+begin
+  Result := False;
+
+  if FTemporaryValue <> EmptyStr then
+  begin
+    FTokenList.Add(TToken.Create(FTemporaryValue ,TTokenType.None));
+    FTemporaryValue := '';
+  end;
+
+  FTokenList.Add(TToken.Create(CurrentValue ,TTokenType.Colon));
+
+  Next;
+  FCurrentState := BranchState;
+end;
+
 constructor TTokenize.Create;
 begin
+  FTokenList := TTokenList.Create;
   FValue := '';
+  FTemporaryValue := '';
   FIndex := 1;
 end;
 
@@ -50,6 +85,31 @@ begin
   inherited;
 end;
 
+function TTokenize.BranchState: Boolean;
+var
+  S: string;
+begin
+  Result := False;
+
+  if CanNext then
+  begin
+
+    S := CurrentValue;
+
+    case WhatIsTokenType(CurrentValue) of
+      TTokenType.SemiColon: FCurrentState := SemiColonState;
+      TTokenType.Colon: FCurrentState := ColonState;
+      TTokenType.Space: FCurrentState := SpaceState;
+      TTokenType.Equal: FCurrentState := EqualState;
+      TTokenType.Operator: FCurrentState := OperatorState;
+      TTokenType.Return : FCurrentState := ReturnState;
+      else FCurrentState := UndefinedState;
+    end;
+  end
+  else
+    FCurrentState := FinalState;
+end;
+
 function TTokenize.CanNext: Boolean;
 begin
   if FIndex > Length(FValue) then
@@ -60,10 +120,57 @@ end;
 
 procedure TTokenize.Next;
 begin
-  Inc(FIndex);
+  if CanNext then
+    Inc(FIndex);
 end;
 
-function TTokenize.Print: string;
+function TTokenize.OperatorState: Boolean;
+begin
+  Result := False;
+
+  FTokenList.Add(TToken.Create(CurrentValue ,TTokenType.Operator));
+
+  Next;
+  FCurrentState := BranchState;
+end;
+
+function TTokenize.ReturnState: Boolean;
+begin
+  Next;
+  FCurrentState := BranchState;
+end;
+
+function TTokenize.SemiColonState: Boolean;
+begin
+  Result := False;
+
+  if FTemporaryValue <> EmptyStr then
+  begin
+    FTokenList.Add(TToken.Create(FTemporaryValue ,TTokenType.None));
+    FTemporaryValue := '';
+  end;
+
+  FTokenList.Add(TToken.Create(CurrentValue ,TTokenType.SemiColon));
+
+  Next;
+  FCurrentState := BranchState;
+end;
+
+function TTokenize.SpaceState: Boolean;
+begin
+  Result := False;
+
+  if FTemporaryValue <> EmptyStr then
+  begin
+    FTokenList.Add(TToken.Create(FTemporaryValue ,TTokenType.None));
+    FTemporaryValue := '';
+  end;
+
+  Next;
+  FCurrentState := BranchState;
+end;
+
+function TTokenize.ToString: string;
 var
   I: Integer;
   TokenList: TTokenList;
@@ -72,110 +179,57 @@ begin
   S := '';
 
   for I := 0 to FTokenList.Count - 1 do
-    S := S + FTokenList.Items[I].Value + ' [' + GetEnumName(Typeinfo(TTokenType),Ord(FTokenList.Items[I].FTokenType)) + '] ';
+    S := S + FTokenList.Items[I].Value + ' [' + GetEnumName(Typeinfo(TTokenType),Ord(FTokenList.Items[I].TokenType)) + '] ';
 
   Result := S;
 end;
 
-function TTokenize.Tokenize: TTokenList;
-var
-  I: Integer;
-  S: string;
-  Token: TToken;
+function TTokenize.EqualState: Boolean;
 begin
+  Result := False;
 
-  Result := TTokenList.Create;
-  S := '';
+  FTokenList.Add(TToken.Create(CurrentValue ,TTokenType.Operator));
+
+  Next;
+  FCurrentState := BranchState;
+end;
+
+function TTokenize.UndefinedState: Boolean;
+begin
+  Result := False;
+
+  FTemporaryValue := FTemporaryValue + CurrentValue;
+
+  Next;
+  FCurrentState := BranchState;
+end;
+
+function TTokenize.Execute(Value: string): TTokenList;
+begin
+  FValue := Value;
+
+  FCurrentState := InitialState;
 
   repeat
-    case WhatIsTokenType(CurrentValue) of
-      TTokenType.Space:
-      begin
-        if S <> EmptyStr then
-        begin
-          Token := TToken.Create;
-          Token.FValue := S;
-          Token.FTokenType := TTokenType.None;
+    FCurrentState;
+  until FCurrentState = FinalState;
 
-          Result.Add(Token);
+  Result := FTokenList;
+end;
 
-          S := '';
-        end;
-      end;
-      TTokenType.SemiColon:
-      begin
-        if S <> EmptyStr then
-        begin
-          Token := TToken.Create;
-          Token.FValue := S;
-          Token.FTokenType := TTokenType.None;
+function TTokenize.FinalState: Boolean;
+begin
+  Result := True;
+end;
 
-          Result.Add(Token);
+function TTokenize.InitialState: Boolean;
+begin
+  Result := False;
 
-          S := '';
-        end;
-
-        Token := TToken.Create;
-        Token.FValue := CurrentValue;
-        Token.FTokenType := TTokenType.SemiColon;
-
-        Result.Add(Token);
-      end;
-      TTokenType.Colon:
-      begin
-        if S <> EmptyStr then
-        begin
-          Token := TToken.Create;
-          Token.FValue := S;
-          Token.FTokenType := TTokenType.None;
-
-          Result.Add(Token);
-
-          S := '';
-        end;
-
-        Token := TToken.Create;
-        Token.FValue := CurrentValue;
-        Token.FTokenType := TTokenType.Colon;
-
-        Result.Add(Token);
-      end;
-      TTokenType.Equal:
-      begin
-        Token := TToken.Create;
-        Token.FValue := CurrentValue;
-        Token.FTokenType := TTokenType.Equal;
-
-        Result.Add(Token);
-      end;
-      TTokenType.Operator:
-      begin
-        Token := TToken.Create;
-        Token.FValue := CurrentValue;
-        Token.FTokenType := TTokenType.Operator;
-
-        Result.Add(Token);
-      end
-      else
-      begin
-        S := S + CurrentValue;
-
-        if WhatIsTokenType(S) = TTokenType.Return then
-        begin
-          Token := TToken.Create;
-          Token.FValue := S;
-          Token.FTokenType := TTokenType.Return;
-
-          Result.Add(Token);
-
-          S := '';
-        end;
-
-      end;
-    end;
-
-    Next;
-  until not CanNext;
+  if FValue = EmptyStr then
+    FCurrentState := FinalState
+  else
+    FCurrentState := BranchState;
 end;
 
 end.
